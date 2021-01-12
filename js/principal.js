5c4fa6db00459b84708ee4a86c0ec762ccdfae6a
oliverso rodas <oliverso91@gmail.com>
2021-01-12 12:36:44 -0600

    cambios presentacion

--- js/principal.js
+++ js/principal.js
@@ -1,4 +1,4 @@
 $(document).ready(function () {
 
-  document.getElementById("nombreUsuario").innerHTML = '<a class="nav-link" style="color: white;">'+nombre+ ' '+rol+'</a>';
+  document.getElementById("nombreUsuario").innerHTML = '<a class="nav-link" style="color: white;">'+nombre+ ' '+apellido+'</a>';
 });
