8706d7522949beb2f22a4fe6d925085b0c8d53e2
Javier Hevia <javierhc22@gmail.com>
2021-01-21 17:36:44 -0600

    paciente y profesor

---
+++ js/bancoProfesor.js
@@ -0,0 +1,61 @@
+$(document).ready(function () {
+  $('#table1').DataTable()
+})
+
+function loading () {
+  $.ajax({
+    type: 'GET',
+    url: dominio + 'users/state2/',
+    contentType: 'application/json',
+    dataType: 'json',
+    crossDomain: true,
+    async: false,
+    success: function (data) {
+      console.log(data)
+      for (const value of data.usuarios) {
+        // console.log(value)
+        var fila = ''
+        fila += '<tr>'
+        fila += '<td>' + value.nombres + '</td>'
+        fila += '<td>' + value.apellidos + '</td>'
+        fila += '<td>' + value.carne + '</td>'
+        fila += '<td>' + value.area + '</td>'
+        fila += '<td>' + value.subarea + '</td>'
+
+        fila += '<td>' + '<a href="./profesorVisualizacion.html?usr=' + value.idusuario + '" target="_blank"> <img src="icon/user.png"></img></a>' + '&nbsp;&nbsp;' +
+                         '<a href="./profesorActualizacion.html?usr=' + value.idusuario + '" target="_blank"><img src="icon/edit.png"></img></a>' + '</td>'
+        fila += '</tr>'
+        var btn = document.createElement('TR')
+        btn.innerHTML = fila
+        document.getElementById('tablita').appendChild(btn)
+      }
+    }
+  })
+}
+
+function loading2 () {
+  $.ajax({
+    type: 'GET',
+    url: dominio + 'patients/state1/',
+    contentType: 'application/json',
+    dataType: 'json',
+    crossDomain: true,
+    async: false,
+    success: function (data) {
+      // console.log(data)
+      for (const value of data.pacientes) {
+        // console.log(value)               value.idpaciente
+        var fila = ''
+        fila += '<tr>'
+        fila += '<td>' + value.nombres + '</td>'
+        fila += '<td>' + value.dpi + '</td>'
+        fila += '<td>' + '<a href="./pacienteVisualizacion.html?usr=' + value.idpaciente + '" target="_blank"> <img src="icon/user.png"></img></a>' + '&nbsp;&nbsp;' +
+                         '<a href="https://www.w3schools.com" target="_blank"><img src="icon/table.png"></img></a>' + '&nbsp;&nbsp;' + '</td>'
+        fila += '</tr>'
+        var btn = document.createElement('TR')
+        btn.innerHTML = fila
+        document.getElementById('tablita').appendChild(btn)
+      }
+    }
+  })
+}
---
+++ js/estudianteActualizacion.js
@@ -0,0 +1,74 @@
+var usuarios = ''
+function loading () {
+  const urlParams = new URLSearchParams(window.location.search)
+  const profesor = urlParams.get('usr')
+  // alert(usuario)
+
+  data = JSON.stringify({
+    id: profesor
+  })
+
+  $.ajax({
+    type: 'POST',
+    url: dominio + 'users/search/',
+    contentType: 'application/json',
+    dataType: 'json',
+    crossDomain: true,
+    async: false,
+    data: data,
+    success: function (data) {
+      // console.log(data.paciente)
+      document.getElementById('nombre').value = data.paciente.nombres
+      document.getElementById('apellido').value = data.paciente.apellidos
+      document.getElementById('dpi').value = data.paciente.cui
+      document.getElementById('telefono').value = data.paciente.telefono
+      document.getElementById('fechanacimiento').value = data.paciente.nacimiento
+      if (data.paciente.genero === 1) {
+        document.getElementById('genero2').checked = true
+      } else {
+        document.getElementById('genero').checked = true
+      }
+      document.getElementById('regestudiante').value = data.paciente.carne
+      usuarios = data.paciente.idusuario
+    }
+  })
+}
+
+function actualizarRegistro () {
+  nombres = $('#nombre').val()
+  apellidos = $('#apellido').val()
+  genero = $("input[name='gender']:checked").val()
+  fechanacimiento = $('#fechanacimiento').val()
+  dpi = $('#dpi').val()
+  telefono = $('#telefono').val()
+  carne = $('#regestudiante').val()
+
+  data = JSON.stringify({
+    nombres: nombres,
+    apellidos: apellidos,
+    genero: genero,
+    nacimiento: fechanacimiento,
+    cui: dpi,
+    telefono: telefono,
+    carne: carne,
+    area: '',
+    subarea: '',
+    id: usuarios
+  })
+  $.ajax({
+    type: 'POST',
+    url: dominio + 'users/update/',
+    contentType: 'application/json',
+    dataType: 'json',
+    crossDomain: true,
+    async: false,
+    data: data,
+    success: function (data) {
+      console.log(data)
+    }
+  })
+}
+
+$('#actualizacion').on('click', function () {
+  actualizarRegistro()
+})
