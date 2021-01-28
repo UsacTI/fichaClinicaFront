88ecfb8177fead97c124f2f06723e9c3f505d524
BryanMG <eddyforte@hotmail.com>
2021-01-27 20:58:00 -0600

    Salas inserts

--- js/expediente.js
+++ js/expediente.js
@@ -82,10 +82,124 @@ function loadHOA() {
     }
 }
 
+function loadData(id) {
+    $.ajax({
+        type: 'GET',
+        url: dominio + `expediente/search/${id}`,
+        contentType: 'application/json',
+        dataType: 'json',
+        crossDomain: true,
+        async: false,
+        //data: data,
+        success: function (data) {
+            console.log(data)
+            let expediente = data['expediente'];
+            document.getElementById('mc').value = expediente['mc'];
+            document.getElementById('hpe').value = expediente['hpe'];
+            let hma = String(expediente['hma']).split(',');
+            let listaHma = document.getElementsByName('hma');
+            document.getElementById('hma-comentarios').value = expediente['hma_comentario'];
+            hma.forEach(element => {
+               //console.log(element);
+                listaHma.forEach(item => {
+                    let idHMA = String(item.id).replace('hma-','');
+                    if (Number(element) == Number(idHMA)) {
+                        //console.log(idHMA); 
+                        item.checked = true;   
+                    }
+                }); 
+            });
+            let hoa = String(expediente['hoa']).split(',');
+            let listaHoa = document.getElementsByName('hoa');
+            document.getElementById('hoa-comentarios').value = expediente['hoa_comentario'];
+            hoa.forEach(element => {
+                listaHoa.forEach(item => {
+                    let idHMA = String(item.id).replace('hoa-','');
+                    if (Number(element) == Number(idHMA)) {
+                        item.checked = true;   
+                    }
+                }); 
+            });
+            let lista_dolor  = expediente['dolor_dentario'];
+            let dolor_dentario = [];
+            let columna = [];
+            for (let i = 0; i < lista_dolor.length; i++) {
+                //console.log(lista_dolor[i]);
+                if (lista_dolor[i] === ',') {
+                    columna.push(lista_dolor[i-1])
+                }else if (lista_dolor[i] === ';') {
+                    dolor_dentario.push(columna);
+                    columna = [];
+                }
+            }
+            console.log(dolor_dentario);
+            let fila = document.getElementById('tabla-hoa').getElementsByTagName('tr');
+            for (let i = 0; i < fila.length; i++) {
+                let tds = fila[i].getElementsByTagName('td');
+                for (let n = 1; n < tds.length; n++) {
+                    tds[n].textContent = dolor_dentario[i][n-1];
+                }
+            }
+
+            document.getElementById('habitos').value = expediente['habitos'];
+            document.getElementById('roentgenologica').value = expediente['roentgenologia'];
+            document.getElementById('precauciones').value = expediente['precauciones'];
+            document.getElementById('evaluacion').value = expediente['evaluacion_clinica'];
+
+            let oclusion = String(expediente['oclusion']).split(',');
+            let listaOclusion = document.getElementsByName('oclusion');
+            document.getElementById('oclusion-descripcion').value = expediente['oclusion_comentario'];
+            oclusion.forEach(element => {
+                listaOclusion.forEach(item => {
+                    let idOclusion = String(item.id).replace('oclusion-','');
+                    if (Number(element) == Number(idOclusion)) {
+                        item.checked = true;   
+                    }
+                }); 
+            });
+
+            let roentoenogramas = String(expediente['roentoenogramas']).split(',');
+            let lista_ordenes_roen = document.getElementsByName('ordenes-roen');
+            document.getElementById('roentoenogramas-descripcion').value = expediente['oclusion_comentario'];
+            roentoenogramas.forEach(element => {
+                lista_ordenes_roen.forEach(item => {
+                    let idOrdenes = String(item.id).replace('orden-','');
+                    if (Number(element) == Number(idOrdenes)) {
+                        item.checked = true;   
+                    }
+                }); 
+            });
+            let opciones = String(expediente['opciones']).split(',');
+            let lista_opciones = document.getElementsByName('opciones');
+            document.getElementById('opciones-descripcion').value = expediente['opciones_descripcion'];
+            opciones.forEach(element => {
+                lista_opciones.forEach(item => {
+                    let idOpcion = String(item.id).replace('op-','');
+                    if (Number(element) == Number(idOpcion)) {
+                        item.checked = true;   
+                    }
+                }); 
+            });
+            document.getElementById('consulta-descripcion').value = expediente['consulta'];
+            if (expediente['consulta'] != '') {
+                document.getElementById('consulta').checked = true;
+            }
+            document.getElementById('estudios-especiales').value = expediente['estudios_especiales'];
+            if (expediente['estudios_especiales'] != '') {
+                document.getElementById('estudios').checked = true;
+            }
+            document.getElementById('equipo-diagnostico').value = expediente['equipo_diagnostico'];
+            }
+        })
+}
+let idExpediente = 0;
 
 $(document).ready(function() {
     loadHMA();
     loadHOA();
+    const urlParams = new URLSearchParams(window.location.search)
+    idExpediente = urlParams.get('id');
+    loadData(idExpediente);
 } )
 
 function addColumn() {
@@ -120,7 +234,7 @@ function guardarExpediente() {
             hma+=element.id.replace('hma-','')+','    
         }
     });
-    console.log(hma);
+    //console.log(hma);
 
     let listaHoa = document.getElementsByName('hoa');
     let hoa = '';
@@ -129,7 +243,7 @@ function guardarExpediente() {
             hoa+=element.id.replace('hoa-','')+','    
         }
     });
-    console.log(hoa);
+    //console.log(hoa);
 
     let listaOclusion = document.getElementsByName('oclusion');
     let oclusion = '';
@@ -138,7 +252,7 @@ function guardarExpediente() {
             oclusion+=element.id.replace('oclusion-','')+','    
         }
     });
-    console.log(oclusion);
+    //console.log(oclusion);
 
     let fila = document.getElementById('tabla-hoa').getElementsByTagName('tr');
     let tabla_hoa = [[],[],[],[],[],];
@@ -158,7 +272,7 @@ function guardarExpediente() {
         }
     }
     //console.log(tabla_hoa);
-    console.log(dolor_dentario);
+    //console.log(dolor_dentario);
     //console.log(document.getElementsByName('ordenes')[0].checked);
     //console.log(document.getElementsByName('hma'));
     //console.log(document.getElementsByName('hoa'));
@@ -182,39 +296,62 @@ function guardarExpediente() {
     //console.log(evaluacion);
     let oclusion_descripcion = document.getElementById('oclusion-descripcion').value;
     //console.log(oclusion_descripcion);
-    //let roentoenogramas = oclusion.getElementById('roentoenogramas').value;
+    let lista_ordenes_roen = document.getElementsByName('ordenes-roen');
+    let roentoenogramas = '';
+    lista_ordenes_roen.forEach(element => {
+        if (element.checked) {
+            roentoenogramas+=element.id.replace('orden-','')+','    
+        }
+    });
+    //console.log(ordenes_roen);
+    let roentoenogramas_descripcion = document.getElementById('roentoenogramas-descripcion').value;
     //console.log(roentoenogramas);
-    //let opciones_descripcion = document.getElementById('opciones-descripcion').value;
+    let lista_opciones = document.getElementsByName('opciones');
+    let opciones = '';
+    lista_opciones.forEach(element => {
+        if (element.checked) {
+            opciones+=element.id.replace('op-','')+','    
+        }
+    });
+    let opciones_descripcion = document.getElementById('opciones-descripcion').value;
     //console.log(opciones_descripcion);
-    //let consulta_descripcion = document.getElementById('consulta-descripcion').value;
+    let consulta_descripcion = document.getElementById('consulta-descripcion').value;
     //console.log(consulta_descripcion);
-    //let estudios_especiales = document.getElementById('estudios-especiales').value;
+    let estudios_especiales = document.getElementById('estudios-especiales').value;
     //console.log(estudios_especiales);
     let equipo_diagnostico = document.getElementById('equipo-diagnostico').value;
     //console.log(equipo_diagnostico);
 
     let expediente = {
-        evaluacion_clinica:  mc,
-        roentgenologia:  roentgenologica,
-        precauciones:  precauciones,
-        equipo_diagnostico:  equipo_diagnostico,
-        habitos:  habitos,
-        diagnostico:  "diagnostico",
-        oclusion:  oclusion,
-        oclusion_comentario:  oclusion_descripcion,
+        mc: mc,
+        hpe: hpe,
         hma:  hma,
         hma_comentario:  hma_comentarios,
         hoa:  hoa,
         hoa_comentario:  hoa_comentarios,
         dolor_dentario:  dolor_dentario,
+        habitos:  habitos,
+        roentgenologia:  roentgenologica,
+        precauciones:  precauciones,
+        evaluacion_clinica:  evaluacion,
+        oclusion:  oclusion,
+        oclusion_comentario:  oclusion_descripcion,
+        roentoenogramas: roentoenogramas,
+        roentoenogramas_descripcion: roentoenogramas_descripcion,
+        opciones: opciones,
+        opciones_descripcion: opciones_descripcion,
+        consulta: consulta_descripcion,
+        estudios_especiales: estudios_especiales,
+        equipo_diagnostico:  equipo_diagnostico,
+        diagnostico:  "",
         idpaciente:  2,
     }
-    console.log(JSON.stringify(expediente));
+    //console.log(JSON.stringify(expediente));
     let data = JSON.stringify(expediente)
-
+    
     $.ajax({
-        type: 'POST',
-        url: dominio + 'Expediente/crear',
+        type: 'PUT',
+        url: dominio + `expediente/update/${idExpediente}`,
         contentType: 'application/json',
         dataType: 'json',
         crossDomain: true,
--- js/pacienteEstudiante.js
+++ js/pacienteEstudiante.js
@@ -1,3 +1,66 @@
 $(document).ready(function() {
   $('#table3').DataTable()
 } )
+
+function goToExpediente(idPaciente) {
+  console.log(idPaciente);
+  $.ajax({
+    type: 'GET',
+    url: dominio + `expediente/all/${idPaciente}`,
+    contentType: 'application/json',
+    dataType: 'json',
+    crossDomain: true,
+    async: false,
+    //data: JSON.stringify({id: id}),
+    success: function (data) {
+      console.log(data);
+      if (data['expediente'].length == 0) {
+        //console.log('hola 1');
+        let expediente = {
+          mc: '',
+          hpe: '',
+          hma: '',
+          hma_comentario: '',
+          hoa: '',
+          hoa_comentario: '',
+          dolor_dentario: '',
+          habitos: '',
+          roentgenologia: '',
+          precauciones: '',
+          evaluacion_clinica: '',
+          oclusion: '',
+          oclusion_comentario: '',
+          roentoenogramas: '',
+          roentoenogramas_descripcion: '',
+          opciones: '',
+          opciones_descripcion: '',
+          consulta: '',
+          estudios_especiales: '',
+          equipo_diagnostico: '',
+          diagnostico:  '',
+          idpaciente:  id,
+        }
+        let data = JSON.stringify(expediente);
+    
+        $.ajax({
+            type: 'POST',
+            url: dominio + 'Expediente/crear',
+            contentType: 'application/json',
+            dataType: 'json',
+            crossDomain: true,
+            async: false,
+            data: data,
+            success: function (data) {
+              console.log(data)
+              location.href = './expediente.html'
+            }
+          })
+      } else if (data['expediente'].length == 1) {
+        //console.log('hola 2');
+        location.href = './expediente.html?id='+data['expediente'][0].idexpediente;
+      } else {
+        console.log('hola 3');
+      }
+    }
+  })
+}
