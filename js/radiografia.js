cc3d05a5875be03d54295a82fb4d6c0436af5989
oli <oliverso91@gmail.com>
2021-02-03 17:09:32 -0600

    nuevas imagenes par carrousell

--- js/radiografia.js
+++ js/radiografia.js
@@ -1,3 +1,16 @@
+$(document).ready(function () {
+
+if(idPersonal == 2){
+
+  $("#cargarRadio").hide();
+}
+else if(idPersonal == 3){
+
+  $("#cargarRadio").show();
+}
+$("#cargarRadio").show();
+
+});
 
 let tratamientos = [];
 let idExpediente = 0;
@@ -30,6 +43,48 @@ function mostrar(){
     reader.readAsDataURL(archivo );
     reader.onloadend = function () {
       document.getElementById("img").src = reader.result;
+      obtenerBase(reader.result);
+
     }
   }
+}
+
+function obtenerBase(base){
+
+  console.log(base.length);
+  testCompression(base, 'deflate');
+
+}
+
+
+function compress(string, encoding) {
+  const byteArray = new TextEncoder().encode(string);
+  const cs = new CompressionStream(encoding);
+  const writer = cs.writable.getWriter();
+  writer.write(byteArray);
+  writer.close();
+  return new Response(cs.readable).arrayBuffer();
+}
+
+function decompress(byteArray, encoding) {
+  const cs = new DecompressionStream(encoding);
+  const writer = cs.writable.getWriter();
+  writer.write(byteArray);
+  writer.close();
+  return new Response(cs.readable).arrayBuffer().then(function (arrayBuffer) {
+    return new TextDecoder().decode(arrayBuffer);
+  });
+}
+
+async function testCompression(text, encoding = 'deflate') {
+  console.log(encoding + ':');
+  console.time('compress');
+  const compressedData = await compress(text, encoding);
+  console.timeEnd('compress');
+  console.log('compressed length:', compressedData.byteLength, 'bytes');
+  console.time('decompress');
+  const decompressedText = await decompress(compressedData, encoding);
+  console.timeEnd('decompress');
+  console.log('decompressed length:', decompressedText.length, 'characters');
+  console.assert(text === decompressedText);
 }
