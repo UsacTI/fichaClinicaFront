837bce526f1ab486b59aa37ff48f174903563b68
BryanMG <eddyforte@hotmail.com>
2021-02-05 18:27:20 -0600

    aprobacion profesro

--- js/profesorAprobacion.js
+++ js/profesorAprobacion.js
@@ -1,3 +1,30 @@
 $(document).ready(function() {
-  $('#table6').DataTable()
+  $('#table6').DataTable();
+  getEstudiantes()
 } )
+
+function getEstudiantes() {
+  document.getElementById('tabla-aprobacion').getElementsByTagName('tr')[0].remove();
+  
+  $.ajax({
+    type: 'GET',
+    url: dominio + 'BuscarDetallePacienteUsuario/'+0,
+    contentType: 'application/json',
+    dataType: 'json',
+    crossDomain: true,
+    async: false,
+    success: function (data) {
+      //console.log(data)
+      data.detalle.forEach(element => {
+        let fila = document.createElement('tr');
+        fila.innerHTML = `
+          <td>${element.nombresUs} ${element.apellidoUs}</td>
+          <td>${element.nombres} ${element.apellidos}</td>
+          <td> <a href="./expedienteProfesor.html?id=${element.idexpediente}"><img src="icon/check-list.png"></> </td>
+        `
+        document.getElementById('tabla-aprobacion').appendChild(fila);  
+      });
+      
+    }
+  })
+}
